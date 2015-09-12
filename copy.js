50e4b561c608d9b1b0fca23038edfcf27345b342
progre <djyayutto@gmail.com>
2015-09-12 14:59:28 +0900

    update

--- copy.js
+++ copy.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 module.exports = function (opts) {
     opts = opts || {};
-    opts.src = opts.src || ['src/**/*', '!tsconfig.json', '!**/*.ts'];
+    opts.src = opts.src || ['src/**/*', '!**/tsconfig.json', '!**/*.*(jade|stylus|ts)'];
     opts.dest = opts.dest || 'lib/';
 
     gulp.task('copy:copy', function () {
