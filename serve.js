4b0d2c7be7ffdb2b29378eae3d1e3fcaa287856f
progre <djyayutto@gmail.com>
2016-03-03 07:56:36 +0900

    Fix server

--- serve.js
+++ serve.js
@@ -1,21 +1,19 @@
 import gulp from "gulp";
-import {exec} from "child_process";
+import {fork} from "child_process";
 let server;
 
 gulp.task("serve:serve", async () => {
     if (server != null) {
-        await (new Promise((resolve, reject) => {
-            server.on("close", resolve);
-            server.on("error", reject);
-            server.stdin.end();
-            server.kill("SIGKILL");
-        }));
+        await stopServer();
     }
-    server = exec("node", ["."]);
-    server.stdout.on("data", data => {
-        console.log(data.toString());
-    });
-    server.stderr.on("data", data => {
-        console.error(data.toString());
-    });
+    server = fork(".");
 });
+
+function stopServer() {
+    return new Promise((resolve, reject) => {
+        server.on("exit", resolve);
+        server.on("error", reject);
+        server.kill("SIGINT");
+        server = null;
+    });
+}
