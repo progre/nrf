c60db14bbcac0138d5b85036914044fd19f59ac9
progre <djyayutto@gmail.com>
2016-03-02 21:30:44 +0900

    Add cache

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import del from "del";
 import "./gulp/copy";
 import "./gulp/jade";
 import "./gulp/selflint";
+import "./gulp/serve";
 import "./gulp/stylus";
 import "./gulp/test";
 import "./gulp/ts";
@@ -39,12 +40,35 @@ gulp.task("release-build",
 );
 
 gulp.task("watch", () => {
-    let opts = { debounceDelay: 3 * 1000 };
+    let signal = false;
+
     gulp.watch("src/**/*.js", gulp.series(begin, "copy:copy", end));
-    gulp.watch(["src/**/*.ts*", "!src/test/**"], opts, gulp.series(begin, "ts:debug", "test:test", end));
+    gulp.watch(["src/**/*.ts*", "!src/test/**"], gulp.series(begin, "ts:debug", "test:test", end));
     gulp.watch("src/**/*.jade", gulp.series(begin, "jade:debug", end));
     gulp.watch("src/**/*.stylus", gulp.series(begin, "stylus:stylus", end));
-    gulp.watch("src/test/**/*.ts", opts, gulp.series(begin, "test:test", end));
+    gulp.watch("src/test/**/*.ts", gulp.series(begin, "test:test", end));
+
+    function begin(callback) {
+        if (signal) {
+            callback(new Error("Alread started."));
+            return;
+        }
+        signal = true;
+        setTimeout(() => {
+            signal = false;
+        }, 3 * 1000);
+        console.log("✂─────────────────────────────────────────────────…………");
+        callback();
+    }
+
+    function end(callback) {
+        console.log("   __       __");
+        console.log("   ) \\     / (      .-.    .---.  .---. .-.   .-.");
+        console.log("  )_  \\_V_/  _(     | |   /   __}{_   _}|  `.'  |");
+        console.log("    )__   __(       | `--.\\  {_ }  | |  | |\\ /| |");
+        console.log("       `-'          `----' `---'   `-'  `-' ` `-'");
+        callback();
+    }
 });
 
 gulp.task("default",
@@ -59,14 +83,4 @@ gulp.task("default",
 
 function clean() {
     return del("lib/");
-}
-
-function begin(callback) {
-    console.log("✂─────────────────────────────────────────────────…………");
-    callback();
-}
-
-function end(callback) {
-    console.log(".:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._");
-    callback();
 }
