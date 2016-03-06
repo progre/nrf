fb9c957230fa93f67b91999578d81dcde731a568
progre <djyayutto@gmail.com>
2016-03-06 11:09:43 +0900

    Fix tasks

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ import gulp from "gulp";
 import del from "del";
 import espower from "gulp-espower";
 import gulpMocha from "gulp-mocha";
-import plumber from "gulp-plumber";
 import sourcemaps from "gulp-sourcemaps";
 import typescript from "gulp-typescript";
 
@@ -10,28 +9,25 @@ export let src = "src/test/**/*.ts";
 export let dest = "lib/test/";
 export let configPath = "tsconfig.json";
 
-gulp.task("test:test", (callback) => {
-    clean().then(() => {
-        build().once("end", () => {
-            mocha().once("end", () => {
-                callback();
-            });
-        });
-    });
+gulp.task("test:clean", () => {
+    return del(dest);
 });
 
-function clean() {
-    return del(dest);
-}
-
-function build() {
+gulp.task("test:build", () => {
     return gulp.src(src)
         .pipe(sourcemaps.init())
         .pipe(typescript(createProject()))
         .pipe(espower())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(dest));
-}
+});
+
+gulp.task("test:mocha", () => {
+    return gulp.src(dest + "**/*.js")
+        .pipe(gulpMocha());
+});
+
+gulp.task("test:test", gulp.series("test:clean", "test:build", "test:mocha"));
 
 function createProject() {
     try {
@@ -41,10 +37,4 @@ function createProject() {
     } catch (e) {
         return {};
     }
-}
-
-function mocha() {
-    return gulp.src(dest + "**/*.js")
-        .pipe(plumber())
-        .pipe(gulpMocha());
 }
