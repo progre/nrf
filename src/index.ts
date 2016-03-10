577cebab45036a43792de858d02e54bcf44cad9d
progre <djyayutto@gmail.com>
2016-03-11 06:36:05 +0900

    Replace browserify with webpack

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 /// <reference path="../typings/main.d.ts" />
 try { require("source-map-support").install(); } catch (e) { /* empty */ }
-console.log("It worked.");
+console.log("It works!");
--- src/public/js/app.ts
+++ src/public/js/app.ts
@@ -1,1 +1,2 @@
 /// <reference path="../../../typings/browser.d.ts" />
+console.log("It works!");
