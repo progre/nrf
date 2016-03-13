be6b6023eadd895c24d9795f789a894fd3283b0b
progre <djyayutto@gmail.com>
2016-03-13 13:20:48 +0900

    Update

--- tslint.js
+++ tslint.js
@@ -2,7 +2,7 @@ import gulp from "gulp";
 import stylish from "gulp-tslint-stylish";
 import tslint from "gulp-tslint";
 
-export let src = ["src/**/*.ts?"];
+export let src = ["src/**/*.ts*"];
 
 gulp.task("tslint:tslint", () => {
     return gulp.src(src)
