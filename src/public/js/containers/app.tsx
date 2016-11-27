ee5d58bc38bb8f4cb674a7e5404c9448a92924d2
progre <djyayutto@gmail.com>
2016-11-27 17:13:32 +0900

    Add ga

--- src/public/js/containers/app.tsx
+++ src/public/js/containers/app.tsx
@@ -49,6 +49,9 @@ function mapDispatchToProps(dispatch: Redux.Dispatch<{}>) {
 
         apply(localConfig: LocalConfig, serviceConfigs: ServiceConfig[]) {
             ipcRenderer.send("apply", { localConfig, serviceConfigs });
+            for (let conf of serviceConfigs.filter(x => x.enabled)) {
+                ga("send", "event", "Settings", "Apply", conf.fmsURL, conf.pushBy);
+            }
             dispatch(footerActions.setToNeedApply(false));
         }
     };
