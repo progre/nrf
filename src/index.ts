de103f685e9c154a4f3124f667adaa3ea9cecf6c
progre <djyayutto@gmail.com>
2016-12-24 12:11:03 +0900

    Fix import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 try { require("source-map-support").install(); } catch (e) { /* empty */ }
 import module from "./module";
-const log4js = require("log4js");
-import {app, BrowserWindow} from "electron";
+import * as log4js from "log4js";
+import { app, BrowserWindow } from "electron";
 
 log4js.configure({
     appenders: [{ type: "console", layout: { type: "basic" } }]
