dcf49fedd4ecec79ed104c9c45df9c3d747464c9
progre <djyayutto@gmail.com>
2017-06-28 19:40:51 +0900

    refactor: refactor

--- src/public/js/components/servicesettings/Contents.tsx
+++ src/public/js/components/servicesettings/Contents.tsx
@@ -4,102 +4,106 @@ import { ServiceConfig, ServiceDefinition } from '../../../../domains/valueobjec
 import { ComboBox } from './commons';
 import { PeerCastInformation, TwitchServerSelector } from './specials';
 
-export default function Contents(props: {
+export interface Props {
   definition: ServiceDefinition;
   config: ServiceConfig;
   onEnabledChange(value: boolean): void;
   onFMSURLChange(value: string): void;
   onStreamKeyChange(value: string): void;
   onPushByChange(value: string): void;
-}) {
-  const fmsId = uuid.v4();
-  const streamKeyId = uuid.v4();
-  const pushById = uuid.v4();
-  return (
-    <div className="col-sm-8">
-      <div className="row">
-        <div className="push-sm-3 col-sm-9">
-          <div className="checkbox">
-            <label>
-              <input
-                type="checkbox"
-                style={{ marginRight: '0.5em' }}
-                checked={props.config.enabled}
-                // tslint:disable-next-line:react-this-binding-issue
-                onChange={e => props.onEnabledChange(
-                  (e.target as HTMLInputElement).checked)}
-              />
-              <span>Enable</span>
-            </label>
+}
+
+export default class Contents extends React.Component<Props, {}> {
+  render() {
+    const fmsId = uuid.v4();
+    const streamKeyId = uuid.v4();
+    const pushById = uuid.v4();
+    return (
+      <div className="col-sm-8">
+        <div className="row">
+          <div className="push-sm-3 col-sm-9">
+            <div className="checkbox">
+              <label>
+                <input
+                  type="checkbox"
+                  style={{ marginRight: '0.5em' }}
+                  checked={this.props.config.enabled}
+                  // tslint:disable-next-line:react-this-binding-issue
+                  onChange={e => this.props.onEnabledChange(
+                    (e.target as HTMLInputElement).checked)}
+                />
+                <span>Enable</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <Label htmlFor={fmsId} text="FMS URL" />
+          <div className="col-sm-9">
+            {
+              this.props.definition.name === 'twitch'
+                ? <TwitchServerSelector
+                  id={fmsId}
+                  value={this.props.config.fmsURL}
+                  onFMSURLChange={this.props.onFMSURLChange}
+                />
+                : <TextBox
+                  id={fmsId}
+                  placeholder="rtmp://..."
+                  value={this.props.config.fmsURL}
+                  onValueChange={this.props.onFMSURLChange}
+                />
+            }
+          </div>
+        </div>
+        <div className="row">
+          {
+            this.props.definition.name === 'peercaststation'
+              ? <PeerCastInformation />
+              : (
+                <div>
+                  <Label htmlFor={streamKeyId} text="Stream key" />
+                  <div className="col-sm-9">
+                    <TextBox
+                      id={streamKeyId}
+                      value={this.props.config.streamKey}
+                      onValueChange={this.props.onStreamKeyChange}
+                    />
+                  </div>
+                </div>
+              )
+          }
+        </div>
+        <div className="row">
+          <Label htmlFor={pushById} text="Push by" />
+          <div className="col-sm-9">
+            <ComboBox
+              id={pushById}
+              values={
+                ['Nginx', 'FFmpeg']
+                  .map(x => ({ key: x, value: x.toLowerCase() }))
+              }
+              selectedValue={this.props.config.pushBy}
+              onValueChange={this.props.onPushByChange}
+              disabled={this.props.definition.pushBy != null}
+            />
+          </div>
+        </div>
+        <div style={{ marginTop: '1em' }} className="row">
+          <div className="col-sm-12" style={{ textAlign: 'right' }}>
+            <a
+              href={this.props.definition.url!}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="selectable"
+            >
+              {this.props.definition.url}
+            </a>
           </div>
         </div>
       </div>
-      <div className="row">
-        <Label htmlFor={fmsId} text="FMS URL" />
-        <div className="col-sm-9">
-          {
-            props.definition.name === 'twitch'
-              ? <TwitchServerSelector
-                id={fmsId}
-                value={props.config.fmsURL}
-                onFMSURLChange={props.onFMSURLChange}
-              />
-              : <TextBox
-                id={fmsId}
-                placeholder="rtmp://..."
-                value={props.config.fmsURL}
-                onValueChange={props.onFMSURLChange}
-              />
-          }
-        </div>
-      </div>
-      <div className="row">
-        {
-          props.definition.name === 'peercaststation'
-            ? <PeerCastInformation />
-            : (
-              <div>
-                <Label htmlFor={streamKeyId} text="Stream key" />
-                <div className="col-sm-9">
-                  <TextBox
-                    id={streamKeyId}
-                    value={props.config.streamKey}
-                    onValueChange={props.onStreamKeyChange}
-                  />
-                </div>
-              </div>
-            )
-        }
-      </div>
-      <div className="row">
-        <Label htmlFor={pushById} text="Push by" />
-        <div className="col-sm-9">
-          <ComboBox
-            id={pushById}
-            values={
-              ['Nginx', 'FFmpeg']
-                .map(x => ({ key: x, value: x.toLowerCase() }))
-            }
-            selectedValue={props.config.pushBy}
-            onValueChange={props.onPushByChange}
-            disabled={props.definition.pushBy != null}
-          />
-        </div>
-      </div>
-      <div style={{ marginTop: '1em' }} className="row">
-        <div className="col-sm-12" style={{ textAlign: 'right' }}>
-          <a
-            href={props.definition.url!}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="selectable"
-          >
-            {props.definition.url}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 function Label(props: { htmlFor: string; text: string }) {
